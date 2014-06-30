7cd2c53c2f0f49bd7edc3855272507a05fbed3d2
mgiorda <giordamauro@gmail.com>
2014-06-30 01:38:05 -0300

    Added test properties

--- src/test/java/com/mgiorda/context/StubTest.java
+++ src/test/java/com/mgiorda/context/StubTest.java
@@ -1,12 +1,27 @@
 package com.mgiorda.context;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.testng.annotations.Test;
 
+@Properties("properties/test.properties")
 public class StubTest extends AbstractTest {
+
+	@Value("${suite.env}")
+	private String defaultEnv;
+
+	@Value("${my.property}")
+	private String myProperty;
+
+	@Value("${my.defaultClass.property}")
+	private String defaultClassProperty;
 
 	@Test
 	public void testSomething() {
 		System.out.println("This is my test");
+
+		System.out.println("This is default Env " + defaultEnv);
+		System.out.println("This is myProperty " + myProperty);
+		System.out.println("This is myDefaultClassProperty " + defaultClassProperty);
 	}
 
 }
