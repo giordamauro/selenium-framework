ce8b103730dc0c41f86057fa8604ad14cd932bb4
Mauro Giorda <mgiorda@apigee.com>
2014-07-17 18:27:04 -0300

    Fixed beforeClass exception issue

--- src/main/java/com/mgiorda/context/AbstractTest.java
+++ src/main/java/com/mgiorda/context/AbstractTest.java
@@ -1,5 +1,7 @@
 package com.mgiorda.context;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.testng.annotations.BeforeClass;
@@ -10,26 +12,36 @@ import com.mgiorda.testng.CurrentTestRun;
 
 public abstract class AbstractTest extends com.mgiorda.testng.AbstractTest {
 
+    private static final Log LOGGER = LogFactory.getLog(AbstractTest.class);
+
     private ApplicationContext applicationContext;
 
     @BeforeClass
     public void $beforeClassAutowireSpring() {
 
-        XmlSuite xmlSuite = CurrentTestRun.getXmlSuite();
-        applicationContext = SuiteContexts.getContextForSuite(xmlSuite);
+        try {
 
-        if (applicationContext == null) {
+            XmlSuite xmlSuite = CurrentTestRun.getXmlSuite();
+            applicationContext = SuiteContexts.getContextForSuite(xmlSuite);
 
-            String defaultContext = "classpath*:/" + SuiteConfiguration.DEFAULT_CONTEXT_LOCATION;
-            applicationContext = new GenericXmlApplicationContext(defaultContext);
+            if (applicationContext == null) {
 
-            java.util.Properties defaultProperties = applicationContext.getBean("defaultProperties", java.util.Properties.class);
-            SpringUtil.addProperties(applicationContext, defaultProperties);
+                String defaultContext = "classpath*:/" + SuiteConfiguration.DEFAULT_CONTEXT_LOCATION;
+                applicationContext = new GenericXmlApplicationContext(defaultContext);
 
-            SuiteContexts.registerSuiteContext(xmlSuite, applicationContext);
+                java.util.Properties defaultProperties = applicationContext.getBean("defaultProperties", java.util.Properties.class);
+                SpringUtil.addProperties(applicationContext, defaultProperties);
+
+                SuiteContexts.registerSuiteContext(xmlSuite, applicationContext);
+            }
+
+            ContextUtil.initContext(applicationContext, this);
+
+        } catch (Exception e) {
+
+            LOGGER.warn(String.format("Exception autowiring beforeClass in test %s", this.getClass().getName()), e);
+            throw e;
         }
-
-        ContextUtil.initContext(applicationContext, this);
     }
 
     protected ApplicationContext getApplicationContext() {
